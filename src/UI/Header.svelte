3205256d2635936291e0e73ba18a2fcc11c92b1c
Konrad Baczyński <konrad.baczynski@almar.pl>
2019-09-24 15:41:12 +0200

    initial commit

---
+++ src/UI/Header.svelte
@@ -0,0 +1,26 @@
+
+<style>
+  header {
+    position: fixed;
+	width: 100%;
+	height: 4rem;
+    top: 0;
+    left: 0;
+	background: #cf0056;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	box-shadow: 0 px 4px rgba(0,0,0,0.30);
+  }
+
+  h1 {
+	  color: white;
+	  font-family: 'Montserrat';
+	  margin: 0;
+  }
+</style>
+
+<header>
+  <h1>Let's meet</h1>
+</header>
