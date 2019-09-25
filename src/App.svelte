3f3ed48b58715c677547b9867985c0dcadb9bb53
Konrad Baczyński <konrad.baczynski@almar.pl>
2019-09-25 09:45:50 +0200

    Added new components and styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,104 @@
 <script>
   import Header from "./UI/Header.svelte";
+  import TextInput from "./UI/TextInput.svelte";
+  import Button from "./UI/Button.svelte";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+
+  let title = "",
+    subtitle = "",
+    description = "",
+    imageUrl = "https://picsum.photos/id/1075/1200",
+    address = "",
+    contactEmail = "";
+
+  let meetups = [
+    {
+      id: "m1",
+      title: "Coding Bootcamp",
+      subtitle: "Learn to code in 2 hours",
+      description: "In this meetup, we will learn stuff and it will be great!",
+      imageUrl: "https://picsum.photos/id/1075/1200",
+      address: "Wojska Polskiego 8, 41-205 Sosnowiec",
+      contactEmail: "learn.code@gmail.com"
+    },
+    {
+      id: "m2",
+      title: "Lear Svelte",
+      subtitle: "Learn Svelte!",
+      description: "Great place to get started learning Svelte.",
+      imageUrl: "https://picsum.photos/id/1076/1200",
+      address: "Wojska Polskiego 8, 41-205 Sosnowiec",
+      contactEmail: "svelte.code@test.com"
+    }
+  ];
+
+  function addMeetup() {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      address: address,
+      imageUrl: imageUrl,
+      contactEmail: contactEmail
+    };
+
+    // meetups.push(newMeetup); // does not WORK!
+    meetups = [newMeetup, ...meetups];
+  }
 </script>
 
-<Header/>
+<style>
+  main {
+    margin-top: 5rem;
+  }
+
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
+</style>
+
+<Header />
+
+<main>
+
+  <form on:submit|preventDefault={addMeetup}>
+    <TextInput
+      id="title"
+      label="Title"
+      value={title}
+      on:input={e => (title = e.target.value)} />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      value={subtitle}
+      on:input={e => (subtitle = e.target.value)} />
+    <TextInput
+      id="description"
+      label="Description"
+      value={description}
+      controlType="textarea"
+      on:input={e => (description = e.target.value)} />
+    <TextInput
+      id="address"
+      label="Address"
+      value={address}
+      on:input={e => (address = e.target.value)} />
+    <TextInput
+      id="imageUrl"
+      label="Image URL"
+      value={imageUrl}
+      on:input={e => (imageUrl = e.target.value)} />
+    <TextInput
+      id="contactEmail"
+      label="Contact E-Mail"
+      value={contactEmail}
+      type="email"
+      on:input={e => (contactEmail = e.target.value)} />
+    <Button type="submit" caption="Save" />
+  </form>
+
+  <MeetupGrid {meetups} />
+</main>
---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,39 @@
+<script>
+  import MeetupItem from "./MeetupItem.svelte";
+
+  export let meetups;
+</script>
+
+<style>
+  section {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 0.5rem;
+  }
+
+  @media (min-width: 768px) {
+    section {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+    @media (min-width: 1200px) {
+    section {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+</style>
+
+<section>
+  {#each meetups as meetup}
+    <MeetupItem
+      key={meetup.id}
+      title={meetup.title}
+      subtitle={meetup.subtitle}
+      description={meetup.description}
+      imageUrl={meetup.imageUrl}
+      address={meetup.address}
+      contactEmail={meetup.contactEmail} />
+  {/each}
+</section>
